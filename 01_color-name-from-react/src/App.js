9d19a4234ed5af83676919cd206f7422d0255305
anna <anchaboma+oba@gmail.com>
2019-11-14 16:43:57 +0900

    Use react hook

--- 01_color-name-from-react/src/App.js
+++ 01_color-name-from-react/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import colorList from "./colorList.json";
 
 import "./App.css";
@@ -19,86 +19,70 @@ function ColorNameList(props) {
   );
 }
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sort: "nameAsc", //name,color
-      list: colorList,
-      columnCount: 1
-    };
-    this.nameSortClick = this.nameSortClick.bind(this);
-    this.colorSortClick = this.colorSortClick.bind(this);
-    this.columnCountClick = this.columnCountClick.bind(this);
-  }
-
-  columnCountClick(e) {
-    this.setState({ columnCount: e.target.value });
-  }
-  sortStateSet(sort) {
-    let tmp = colorList.slice();
-    tmp.sort((a, b) => a.name < b.name);
-
-    let _colors = this.state.list.slice();
-    switch (sort) {
-      case "nameAsc":
+function sortList(sortType, list) {
+  let _colors = list.slice();
+  switch (sortType.type) {
+    case "name":
+      if (sortType.asc === true) {
         _colors.sort((a, b) => {
           const x = a.name.toLowerCase();
           const y = b.name.toLowerCase();
           return x < y ? -1 : x > y ? 1 : 0;
         });
-        break;
-      case "nameDesc":
+      } else {
         _colors.sort((b, a) => {
           const x = a.name.toLowerCase();
           const y = b.name.toLowerCase();
           return x < y ? -1 : x > y ? 1 : 0;
         });
-        break;
-      case "color":
-        break;
-      default:
-        break;
-    }
-    this.setState({ sort: sort });
-    this.setState({ list: _colors });
+      }
   }
-  nameSortClick() {
-    const sort = this.state.sort === "nameAsc" ? "nameDesc" : "nameAsc";
-    this.sortStateSet(sort);
-  }
-  colorSortClick() {
+  return _colors;
+}
+
+function App() {
+  const [sortType, setSortType] = useState({ type: "name", asc: false });
+  const [list, setList] = useState(colorList);
+  const [columnCount, setColumn] = useState(1);
+
+  const columnCountClick = useCallback(e => {
+    setColumn(e.target.value);
+  });
+
+  const nameSortClick = useCallback(e => {
+    setSortType({ type: e.target.value, asc: !sortType.asc });
+    setList(sortList(sortType, list));
+  });
+  const colorSortClick = useCallback(() => {
     this.sortStateSet("color");
-  }
-  render() {
-    return (
-      <div className="App">
-        <div>
-          {this.state.sort}
-          <br />
-          {this.state.columnCount}
-          <h1>{colorList.length}colors</h1>
-          <label for="columnInput">Count of columns</label>
-          <input
-            type="number"
-            step="1"
-            min="1"
-            max="10"
-            style={{ width: "3em" }}
-            name="columnINput"
-            value={this.state.columnCount}
-            onChange={this.columnCountClick}
-          />
-          <br />
-          sort: <button onClick={this.nameSortClick}>name</button>
-        </div>
-        <ColorNameList
-          list={this.state.list}
-          columnCount={this.state.columnCount}
+  });
+  return (
+    <div className="App">
+      <div>
+        {sortType.type}/ {sortType.asc.toString()}
+        <br />
+        {columnCount}
+        <h1>{colorList.length}colors</h1>
+        <label for="columnInput">Count of columns</label>
+        <input
+          type="number"
+          step="1"
+          min="1"
+          max="10"
+          style={{ width: "3em" }}
+          name="columnINput"
+          value={columnCount}
+          onChange={columnCountClick}
         />
+        <br />
+        sort:{" "}
+        <button value="name" onClick={nameSortClick}>
+          name
+        </button>
       </div>
-    );
-  }
+      <ColorNameList list={list} columnCount={columnCount} />
+    </div>
+  );
 }
 
 export default App;
