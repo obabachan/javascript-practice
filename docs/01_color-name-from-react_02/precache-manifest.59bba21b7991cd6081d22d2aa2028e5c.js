1a651826d211cf006be14884ffb432fee07efd23
anna <anchaboma+oba@gmail.com>
2019-11-14 21:44:56 +0900

    sort of brightness

---
+++ docs/01_color-name-from-react_02/precache-manifest.59bba21b7991cd6081d22d2aa2028e5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "309195b8b3adf16591bec4ad3d36c4cd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c957638c32bcf813acd4",
+    "url": "./static/css/main.a58d724a.chunk.css"
+  },
+  {
+    "revision": "64629eef77a1715afb4f",
+    "url": "./static/js/2.1d041e97.chunk.js"
+  },
+  {
+    "revision": "c957638c32bcf813acd4",
+    "url": "./static/js/main.eb51d60a.chunk.js"
+  },
+  {
+    "revision": "58859c170cac7f123148",
+    "url": "./static/js/runtime-main.6dbb22a1.js"
+  }
+]);
