4b0418b115955c795735231a8a54bd4c3bdfa165
anna <anchaboma+oba@gmail.com>
2019-11-15 22:22:58 +0900

    add select font color - html

---
+++ docs/01_color-name-from-react_03/precache-manifest.4692c57c53fa9f5d74df97746f7da106.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6095eb245abee5751406318c4c21011",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5b6d0aaee64d044b5424",
+    "url": "./static/css/main.259406f1.chunk.css"
+  },
+  {
+    "revision": "64629eef77a1715afb4f",
+    "url": "./static/js/2.1d041e97.chunk.js"
+  },
+  {
+    "revision": "5b6d0aaee64d044b5424",
+    "url": "./static/js/main.a46d6658.chunk.js"
+  },
+  {
+    "revision": "58859c170cac7f123148",
+    "url": "./static/js/runtime-main.6dbb22a1.js"
+  }
+]);
